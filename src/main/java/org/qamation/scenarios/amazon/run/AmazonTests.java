5a6eadb0cd00ac75ddb74cdefbe0eba5dd0ee71d
Pavel Gouchtchine <pavel.gouchtchine@cantire.com>
2018-12-10 16:32:12 -0500

    simplifying tests.

--- src/main/java/org/qamation/scenarios/amazon/run/AmazonTests.java
+++ src/main/java/org/qamation/scenarios/amazon/run/AmazonTests.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.qamation.scenarios.amazon.AmazonLogin;
 import org.qamation.utils.FileUtils;
 import org.qamation.webdriver.utils.WebDriverFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -11,35 +14,47 @@ import org.testng.annotations.Test;
 import java.util.Properties;
 
 public class AmazonTests {
-    private final String TIME_OUT_PROPERTIES = "/home/pavel/workspace/qamation/documentation/Examples/TIME_OUT.properties";
-    private final String SELENIUM_HOME = "/home/pavel/workspace/Selenium";
+    private static Logger log = LoggerFactory.getLogger(AmazonTests.class);
+    private final String TIME_OUT_PROPERTIES;
+
+    {
+        TIME_OUT_PROPERTIES = "/home/pavel/workspace/qamation/documentation/Examples/TIME_OUT.properties";
+    }
+
+    private final String SELENIUM_HOME = System.getProperty("user.dir")+"/../Selenium";
 
     private WebDriver driver;
 
-    @BeforeTest
+    @BeforeClass
     public void setUp() {
-        Properties timeOutprops = FileUtils.loadPropertiesFile(TIME_OUT_PROPERTIES);
-        System.setProperties(timeOutprops);
-        System.setProperty("webdriver.chrome.driver",SELENIUM_HOME+"/Chrome/chromedriver");
-
+        //Properties timeOutprops = FileUtils.loadPropertiesFile(TIME_OUT_PROPERTIES);
+        //System.setProperties(timeOutprops);
+        System.setProperty("webdriver.chrome.driver",SELENIUM_HOME+"/ChromeDriver/chromedriver.exe");
+        /*
         try {
             ClassLoader.getSystemClassLoader().loadClass("org.slf4j.LoggerFactory");
             ClassLoader.getSystemClassLoader().loadClass("org.slf4j.Logger");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        */
+        driver = WebDriverFactory.createChromeWebDriver();
 
     }
 
     @Test
     public void loginToAmazon() {
         try {
-            driver = WebDriverFactory.createChromeWebDriver();
             AmazonLogin login = new AmazonLogin(driver);
             login.run();
         }
         catch (Throwable th) {
             th.printStackTrace();
         }
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
     }
 }
